219c1e0508b55b9336b53c00473d5e3d961b9d66
norben999 <norben.oriarte@live.com>
2022-05-31 19:06:04 +0800

    Session 4 Demo Codes (#4)

---
+++ EmployeeManagementPortal/EmployeeManagementPortal/Pages/CounterPartialClass.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeManagementPortal.Pages
+{
+    public partial class CounterPartialClass
+    {
+        private int currentCount = 10;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
