c4b20d0de4c3ac5fc4691647b7cf98891c748eb2
norben999 <norben.oriarte@live.com>
2022-06-07 15:44:08 +0800

    Assignment-2

---
+++ SoftvisonBlazorMasters/SoftvisonBlazorMasters/Client/Models/Assignment.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftvisonBlazorMasters.Client.Models
+{
+    public class Assignment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public string Route { get; set; }
+    }
+}
